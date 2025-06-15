3af6cd41cd0f4394f02842e6b0a0f262ff66d713
Sasindu Gihan <sasindugihan123456@gmail.com>
2025-06-15 22:14:22 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello World <br>" ;
+echo "It's really good";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compat">
+	<meta name="viewport" content>
+	<title>Document</title>
+</head>
+<body>
+	<br>
+	<button>Click me</button>
+
+</body>
+</html>
