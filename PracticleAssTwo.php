2d7b73147f524272c948ac4be36ee1d82cf657e0
Sasindu Gihan <sasindugihan123456@gmail.com>
2025-07-01 16:58:39 +0530

    Add files via upload

---
+++ PracticleAssTwo.php
@@ -0,0 +1,10 @@
+<?php
+    $username = "Sasindu";
+    if ($username == "Sasindu") {
+        echo "Welcome to the PHP world, $username!";
+    } else {
+        echo "You are not authorized to access this page.";
+    }
+    
+
+?>
